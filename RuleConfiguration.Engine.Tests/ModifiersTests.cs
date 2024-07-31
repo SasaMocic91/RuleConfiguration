3e13e928d894099a1f9b2929aab1bd0060e38baa
Sasa Mocic <Sasa.Mocic@seavus.com>
2024-07-31 13:05:14 +0200

    Add Modifiers Tests

---
+++ RuleConfiguration.Engine.Tests/ModifiersTests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Mongo2Go;
+using NUnit.Framework;
+using RuleConfiguration.Engine.Tests.FakeData;
+using RuleConfiguration.Engine.Tests.Helpers;
+using RuleConfiguration.Modifiers;
+using RuleConfiguration.Storage;
+using RuleConfigurator.Application.Models;
+
+namespace RuleConfiguration.Engine.Tests;
+
+[TestFixture]
+public class ModifierTests
+{
+    private readonly Guid _tenantId = Guid.NewGuid();
+
+    [Test]
+    public async Task MongoDb_AddNewRule_Tax()
+    {
+        using var runner = MongoDbRunner.Start();
+        var memCache = new MemoryCache(new MemoryCacheOptions());
+        var mongoDb = MongoHelper.GetDb(runner);
+        var rulesCache = new RulesCache(memCache, mongoDb);
+
+        var rule = GenerateFakeRules.GenerateTaxRule(_tenantId);
+        var modifierRepo = new ModifierRepo(rulesCache);
+
+        await mongoDb.CreateRule(rule);
+        await rulesCache.StoreConfiguration(rule.TenantId);
+
+        var ticket = GenerateFakeBettingData.FakeHighTickets(1).Single();
+        ticket.TenantId = _tenantId;
+
+        Assert.That(0, Is.EqualTo(ticket.Bonus));
+
+        await modifierRepo.ApplyModifiers(ticket);
+        Assert.That(0, Is.Not.EqualTo(ticket.Tax));
+    }
+
+    [Test]
+    public async Task MongoDb_AddNewRule_Jackpot()
+    {
+        using var runner = MongoDbRunner.Start();
+        var memCache = new MemoryCache(new MemoryCacheOptions());
+
+        var mongoDb = MongoHelper.GetDb(runner);
+        var rulesCache = new RulesCache(memCache, mongoDb);
+
+        var modifierRepo = new ModifierRepo(rulesCache);
+
+        var rule = GenerateFakeRules.GenerateJackpotRule(_tenantId);
+
+        await mongoDb.CreateRule(rule);
+        await rulesCache.StoreConfiguration(rule.TenantId);
+
+        var ticket = GenerateFakeBettingData.FakeHighTickets(1).Single();
+        ticket.TenantId = _tenantId;
+
+        Assert.That(0, Is.EqualTo(ticket.Bonus));
+
+        await modifierRepo.ApplyModifiers(ticket);
+        Assert.That(0, Is.Not.EqualTo(Jackpot.TotalAmount));
+    }
+
+    [Test]
+    public async Task MongoDb_AddNewRule_Bonus()
+    {
+        using var runner = MongoDbRunner.Start();
+        var memCache = new MemoryCache(new MemoryCacheOptions());
+
+        var mongoDb = MongoHelper.GetDb(runner);
+        var rulesCache = new RulesCache(memCache, mongoDb);
+
+        var modifierRepo = new ModifierRepo(rulesCache);
+
+        var rule = GenerateFakeRules.GenerateBonusRule(_tenantId);
+
+        await mongoDb.CreateRule(rule);
+        await rulesCache.StoreConfiguration(rule.TenantId);
+
+        var ticket = GenerateFakeBettingData.FakeHighTickets(1).Single();
+        ticket.TenantId = _tenantId;
+
+        Assert.That(0, Is.EqualTo(ticket.Bonus));
+        Assert.That(0, Is.EqualTo(ticket.Tax));
+
+        var result = await modifierRepo.ApplyModifiers(ticket);
+        Assert.That(0, Is.Not.EqualTo(result.Bonus));
+    }
+
+    [Test]
+    public async Task MongoDb_AddNewRule_BonusAndJackpot()
+    {
+        using var runner = MongoDbRunner.Start();
+        var memCache = new MemoryCache(new MemoryCacheOptions());
+
+        var mongoDb = MongoHelper.GetDb(runner);
+        var rulesCache = new RulesCache(memCache, mongoDb);
+
+        var modifierRepo = new ModifierRepo(rulesCache);
+
+        var bonusRule = GenerateFakeRules.GenerateBonusRule(_tenantId);
+
+        var jackpotRule = GenerateFakeRules.GenerateJackpotRule(_tenantId);
+
+        await mongoDb.CreateRule(bonusRule);
+        await mongoDb.CreateRule(jackpotRule);
+        await rulesCache.StoreConfiguration(_tenantId);
+
+        var ticket = GenerateFakeBettingData.FakeHighTickets(1).Single();
+        ticket.TenantId = _tenantId;
+
+        Assert.That(0, Is.EqualTo(ticket.Bonus));
+        Assert.That(0, Is.EqualTo(Jackpot.TotalAmount));
+
+        var result = await modifierRepo.ApplyModifiers(ticket);
+        Assert.That(0, Is.Not.EqualTo(result.Bonus));
+        Assert.That(0, Is.Not.EqualTo(Jackpot.TotalAmount));
+    }
+}
