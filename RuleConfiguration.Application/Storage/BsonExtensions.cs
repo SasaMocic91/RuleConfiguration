50104f28f33b07ce4b57d81410dae5e97cb25de3
Sasa Mocic <Sasa.Mocic@seavus.com>
2024-07-31 12:16:10 +0200

    Add Bson Extension and tests

---
+++ RuleConfiguration.Application/Storage/BsonExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using MongoDB.Bson;
+
+namespace RuleConfiguration.Storage;
+
+public static class BsonExtensions
+{
+    public static BsonValue ToBsonValue(this JsonElement e, bool tryParseDateTimes = false)
+    {
+        return e.ValueKind switch
+        {
+            JsonValueKind.String => BsonValue.Create(e.GetString()),
+            JsonValueKind.Number when e.TryGetInt32(out var v) => BsonValue.Create(v),
+            JsonValueKind.Number when e.TryGetDecimal(out var v) => BsonValue.Create(v),
+            JsonValueKind.Null => BsonValue.Create(null),
+            JsonValueKind.True => BsonValue.Create(true),
+            JsonValueKind.False => BsonValue.Create(false),
+            JsonValueKind.Array => new BsonArray(e.EnumerateArray().Select(v => v.ToBsonValue(tryParseDateTimes))),
+            JsonValueKind.Undefined => BsonNull.Value,
+            _ => throw new NotSupportedException($"ToBsonValue: {e}")
+        };
+    }
+}
---
+++ RuleConfiguration.Engine.Tests/BsonExtensionTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using MongoDB.Bson;
+using NUnit.Framework;
+using RuleConfiguration.Storage;
+
+namespace RuleConfiguration.Engine.Tests;
+
+[TestFixture]
+public class ExtensionTests
+{
+    [Test]
+    public void BsonExtension_ReturnsGoodValue()
+    {
+        var decimalValue = JsonSerializer.SerializeToElement(1000.2M);
+        var decimalConverted = decimalValue.ToBsonValue();
+        Assert.That(BsonType.Decimal128, Is.EqualTo(decimalConverted.BsonType));
+
+        var intValue = JsonSerializer.SerializeToElement(1000);
+        var intConverted = intValue.ToBsonValue();
+        Assert.That(BsonType.Int32,Is.EqualTo(intConverted.BsonType));
+
+        var stringValue = JsonSerializer.SerializeToElement("test");
+        var stringConverted = stringValue.ToBsonValue();
+        Assert.That(BsonType.String, Is.EqualTo(stringConverted.BsonType));
+
+        var trueValue = JsonSerializer.SerializeToElement(true);
+        var trueConverted = trueValue.ToBsonValue();
+        Assert.That(BsonType.Boolean, Is.EqualTo(trueConverted.BsonType));
+
+        var falseValue = JsonSerializer.SerializeToElement(false);
+        var falseConverted = falseValue.ToBsonValue();
+        Assert.That(BsonType.Boolean, Is.EqualTo(falseConverted.BsonType));
+
+        var arr = new int[2];
+        var arrayValue = JsonSerializer.SerializeToElement(arr);
+        var arrConverted = arrayValue.ToBsonValue();
+        Assert.That(BsonType.Array, Is.EqualTo(arrConverted.BsonType));
+    }
+}
